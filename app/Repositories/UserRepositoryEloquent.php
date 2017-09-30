da9b2c2f0464841cc9f69ee10cad4457607fe146
Israel Jardim <israjardim21@gmail.com>
2017-09-30 16:35:58 +0000

    Altering book factory and seed

---
+++ app/Repositories/UserRepositoryEloquent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CodePub\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use CodePub\Models\User;
+
+/**
+ * Class UserRepositoryEloquent
+ * @package namespace CodePub\Repositories;
+ */
+class UserRepositoryEloquent extends BaseRepository implements UserRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return User::class;
+    }
+
+    
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+}
