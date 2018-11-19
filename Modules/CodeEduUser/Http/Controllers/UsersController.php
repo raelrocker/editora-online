ad772b477b31af4477cecf1b02cf66360c70b6aa
Israel Jardim <israjardim21@gmail.com>
2018-11-18 23:11:44 -0200

    Modulo CodeEduUser

---
+++ Modules/CodeEduUser/Http/Controllers/UsersController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace CodeEduUser\Http\Controllers;
+
+use CodePub\Criteria\FindByNameCriteria;
+use CodeEduUser\Models\User;
+use CodeEduUser\Http\Requests\UserRequest;
+use CodeEduUser\Repositories\UserRepository;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    public function __construct(UserRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
+        $search = $request->get('search');
+        $this->repository
+            ->pushCriteria(new FindByNameCriteria($search));
+        $users = $this->repository->paginate(10);
+        return view('codeeduuser::users.index', compact('users', 'search'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('codeeduuser::users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param UserRequest|Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserRequest $request)
+    {
+        $this->repository->create($request->all());
+        $url = $request->get('redirect_to', route('codeeduuser.users.index'));
+        $request->session()->flash('message', 'Usuário cadastrado com sucesso.');
+        return redirect()->to($url);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param $id
+     * @return \Illuminate\Http\Response
+     * @internal param User $user
+     */
+    public function edit($id)
+    {
+        $user = $this->repository->find($id);
+        return view('codeeduuser::users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UserRequest|Request $request
+     * @param $id
+     * @return \Illuminate\Http\Response
+     * @internal param User $user
+     */
+    public function update(UserRequest $request, $id)
+    {
+        $this->repository->update($request->all(), $id);
+        $url = $request->get('redirect_to', route('codeeduuser.users.index'));
+        $request->session()->flash('message', 'Usuário alterado com sucesso.');
+        return redirect()->to($url);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param $id
+     * @return \Illuminate\Http\Response
+     * @internal param User $user
+     * @internal param int $id
+     */
+    public function destroy($id)
+    {
+        $this->repository->delete($id);
+        \Session::flash('message', 'Usuário excluído com sucesso.');
+        return redirect()->to(\URL::previous());
+    }
+}
