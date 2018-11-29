eef4c69d979549da89079691e221473c9a039f61
Israel Jardim <israjardim21@gmail.com>
2018-11-28 22:47:54 -0200

    administrador padrão

---
+++ Modules/CodeEduUser/database/migrations/2018_11_29_002724_create_users_data.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \CodeEduUser\Models\User::create([
+            'name' => config('codeeduuser.user_default.name'),
+            'email' => config('codeeduuser.user_default.email'),
+            'password' => bcrypt(config('codeeduuser.user_default.password')),
+            'verified' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $user = \CodeEduUser\Models\User::where('email', 'admin@user.com')->first();
+        $user->forceDelete();
+    }
+}
