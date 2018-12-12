202d71d6ecdbeaff38e3e702405bde3089313d49
Israel Jardim <israel@aucon.com.br>
2018-12-12 14:35:39 -0200

    upload de cover de pdf

--- Modules/CodeEduBook/Models/BookStorageTrait.php
+++ Modules/CodeEduBook/Models/BookStorageTrait.php
@@ -24,4 +24,24 @@ trait BookStorageTrait
     {
         return "{$this->disk}/{$this->ebook_template}/{$this->cover_ebook_name}";
     }
+    
+    public function getCoverPdfNameAttribute()
+    {
+        return 'cover.pdf';
+    }
+    
+    public function getPdfTemplateAttribute()
+    {
+        return "{$this->id}/Resources/Templates/pdf";
+    }
+    
+    public function getPdfTemplateStorageAttribute()
+    {
+        return "{$this->disk}/{$this->pdf_template}";
+    }
+    
+    public function getCoverPdfFileAttribute()
+    {
+        return "{$this->pdf_template_storage}/{$this->cover_pdf_name}";
+    }
 }
--- Modules/CodeEduBook/Pub/BookCoverUpload.php
+++ Modules/CodeEduBook/Pub/BookCoverUpload.php
@@ -10,5 +10,19 @@
      {
         \Storage::disk(config('codeedubook.book_storage'))
                 ->putFileAs($book->ebook_template, $cover, $book->cover_ebook_name);
+        
+        $this->makeCoverPdf($book);
+     }
+     
+     protected function makeCoverPdf(Book $book)
+     {
+         if (!is_dir($book->pdf_template_storage)) {
+             mkdir($book->pdf_template_storage, 0775, true);
+         }
+         
+         $img = new \Imagick($book->cover_ebook_file);
+         $img->setimageformat('pdf');
+         
+         $img->writeimage($book->cover_pdf_file);
      }
  }
